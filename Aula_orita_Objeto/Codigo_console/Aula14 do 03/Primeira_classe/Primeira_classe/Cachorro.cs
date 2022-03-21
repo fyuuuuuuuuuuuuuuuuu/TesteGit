7fb592cd946386e91553fd2381eeb4e6b229aef4
SobekSCP682 <76125359+fyuuuuuuuuuuuuuuuuu@users.noreply.github.com>
2022-03-21 10:45:39 -0300

    Add files via upload

---
+++ Aula_orita_Objeto/Codigo_console/Aula14 do 03/Primeira_classe/Primeira_classe/Cachorro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primeira_classe
+{
+    class Cachorro
+    {
+        //Atributos
+        public string nome;
+        public string raça;
+        public int idade;
+        public float peso;
+
+        //Metodos
+        public void latir()
+        {
+            Console.WriteLine($"{nome} esta latindo");
+        }
+
+        public void abanar()
+        {
+            Console.WriteLine($"{nome} esta animado");
+        }
+    }
+}
